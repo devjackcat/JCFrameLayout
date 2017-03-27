3c21b74e41b4bd240c6a7321bd0a3df2d6f3852b
wangyp2018 <wangyp2018@163.com>
2017-03-27 17:25:40 +0800

    初始化项目

---
+++ JCFrameLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCFrameLayout
+//
+//  Created by abc on 17/3/27.
+//  Copyright © 2017年 jackcat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCFrameLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCFrameLayout
+//
+//  Created by abc on 17/3/27.
+//  Copyright © 2017年 jackcat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
